d00395330fb2e8a09ec39585928c256ebd2316c0
Thomas Eapen <thomas@nowlabs.net>
2010-04-29 10:45:36 -0400

    Preparing for the iTunesFinder project

--- Chapter 12.m
+++ Chapter 12.m
@@ -1,17 +1,8 @@
 #import <Foundation/Foundation.h>
-#import "NumberConvenience.h"
+
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	
-	NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-	[dict setObject:[@"hello" lengthAsNumber]
-			 forKey:@"hello"];
-	[dict setObject:[@"iPod Touch" lengthAsNumber]
-			 forKey:@"iPod Touch"];
-	[dict setObject:[@"Once upon a time" lengthAsNumber]
-			 forKey:@"Once upon a time"];
-	NSLog(@"%@", dict);
 	
     [pool drain];
     return 0;
